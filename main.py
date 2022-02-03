9bf60a9fd14f27dc1917a243a0eef79c1f7463ee
rhuaan-eq <97266278+rhuaan-eq@users.noreply.github.com>
2022-02-03 00:44:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,9 @@ def caesar(chosen_direction,text_typed,shift_amount):
     
     print(f'The {chosen_direction}d message is: {final_text}\n')
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 loop = True
 while loop == True:
-
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     print(logo)
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     while direction != 'encode' and 'decode':
